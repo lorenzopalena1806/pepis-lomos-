2a25ab15e7e3c9f895a88d0e7b40c8ede5c4a306
Lorenzo <lorenzopalena1806@gmail.com>
2026-05-20 18:09:44 -0300

    Cambiando a HashRouter para evitar error 404

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import StoreFront from './StoreFront';
 import KitchenView from './components/KitchenView';
 import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<StoreFront />} />
         <Route path="/cocina" element={<KitchenView />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
